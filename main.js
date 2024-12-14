ca80f45f6142adeb4c10b48110646cce20a17211
bruno <bruno.b.fernandes12@hotmail.com>
2024-12-14 15:42:22 -0300

    Adding footer and new js feature

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     $('#store-image').slick({
         autoplay: true
     })
+
+    $('#price').on('input', function () {
+        const price = $(this).val();
+        $('#price-value').text(`R$${price}`);
+    });
 })
